aaaa918dc4fc770fa40a2f8905f49935c906c556
RomanenkoStud <pavlo.romanenko@nure.ua>
2022-08-30 11:38:08 +0300

    modified:   css/style.css 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ console.log('Hello world!');
 document.getElementById('button').addEventListener("click", function() {
     alert("Congrats! You can move out, your house is sold.");
 });
+
+$(document).ready(function() {
+    $('#preloader').fadeOut(4000);
+});
